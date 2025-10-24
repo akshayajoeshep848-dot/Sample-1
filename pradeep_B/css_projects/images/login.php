5bcecf38ef7d9b429517ccac43589040d95876af
pradeep <pradeepbarigala@gmail.com>
2025-10-24 18:43:54 +0530

    added folder pradeep

---
+++ pradeep_B/css_projects/images/login.php
@@ -0,0 +1,436 @@
+<!DOCTYPE html>
+<html lang="zxx">
+  <head>
+    <!-- meta tag -->
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>LogicWhile: Build the best skill set for securing an entry-level job.</title>
+    <meta name="robots" content="index, follow">
+    <meta name="description" content="Logic While offers comprehensive services including Academic Training, CRT (Campus Recruitment Training), Internships, SoC (Skill-oriented Course), Projects, and Workshops to enhance skills and career growth.">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="canonical" href="http://logicwhile.com" />
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
+    <!-- Meta tags -->
+<meta name="keywords" content="Logic While, Logicwhile, Academic Training, CRT, Campus Recruitment Training, Internships, SoC, Skill-oriented Course, Projects, Workshops" />
+<meta name="description" content="Logic While offers comprehensive services including Academic Training, CRT (Campus Recruitment Training), Internships, SoC (Skill-oriented Course), Projects, and Workshops to enhance skills and career growth." />
+<meta name="author" content="Logic While" />
+<meta name="robots" content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large" />
+<link rel="canonical" href="https://www.logicwhile.com" />
+
+<!-- Google Search Console Verification -->
+<meta name="google-site-verification" content="xj4WIZCRX9vs9MLF4HPB8k-_JBfJh6lguqZPr84H1fc" />
+    <meta property="og:locale" content="en_US">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Logic While">
+    <meta property="og:description" content="Logic While">
+    <meta property="og:url" content="Logic While">
+    <meta property="og:site_name" content="Logic While">
+    <meta property="og:updated_time" content="2021-04-13T14:03:56+00:00">
+    <meta property="og:image" content="assest/img/logo.png">
+    <meta property="og:image:secure_url" content="assets/img/logo.png">
+    <meta property="og:image:width" content="521">
+    <meta property="og:image:height" content="210">
+    <meta property="og:image:alt" content="Homepage">
+    <meta property="og:image:type" content="assets/img/png">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Homepage - Logic While">
+    <meta name="twitter:description" content="Logic While">
+    <meta name="twitter:image" content="assets/img/logo.png">
+    <link rel="apple-touch-icon" href="#">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/animate.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/owl.carousel.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/slick.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/f5.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/off-canvas.css">
+    <link rel="stylesheet" type="text/css" href="assets/fonts/linea-fonts.css">
+    <link rel="stylesheet" type="text/css" href="assets/fonts/flaticon.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/rsmenu-main.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/rs-spacing.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
+  </head>
+  <body class="defult-home">
+        <!--Preloader area start here-->
+    <!-- <div id="loader" class="loader orange-color">
+      <div class="loader-container">
+        <div class='loader-icon'>
+          <img src="assets/img/logo.png" alt="">
+        </div>
+      </div>
+    </div> -->
+    <!--Preloader area End here-->
+    <!-- Main content Start -->
+    <div class="main-content">
+      <!--Full width header Start-->
+      <div class="full-width-header home8-style4 main-home">
+        <!--Header Start-->
+        <header id="rs-header" class="rs-header">
+          <!-- Menu Start -->
+          <div class="menu-area menu-sticky">
+            <div class="container">
+              <div class="row y-middle">
+                <div class="col-lg-3">
+                  <div class="logo-cat-wrap">
+                    <div class="logo-part">
+                      <a href="index.php">
+                        <img class="normal-logo pt-0" src="assets/img/new-logo.png" alt="logo">
+                        <img class="sticky-logo" src="assets/img/new-logo.png" alt="logo">
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-9 text-right">
+                  <div class="rs-menu-area">
+                    <div class="main-menu">
+                      <div class="mobile-menu">
+                        <div id="mySidenav" class="sidenav">
+                          <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+                          <ul>
+                            <li> <a href="#">Home</a></li>
+                            <li> <a href="about.php">About Us</a></li>
+                            <li><a href="service.php">Services</a></li>
+                            <li><a href="platform.php">Platform</a></li>
+                            <li><a href="contact.php">Contact Us</a></li>
+                            <li><a href="login.php">Login</a></li>
+                          </ul>
+                        </div>
+                        <span style="font-size:30px;cursor:pointer; line-height: 85px; color: #00006c" class="d-lg-none d-block" onclick="openNav()">&#9776;</span>
+                      </div>
+                      <nav class="rs-menu">
+                        <ul class="nav-menu">
+                          
+                          <li >
+                            <a href="index.php">Home</a>
+                          </li>
+                          <li >
+                            <a href="about.php">About Us</a>
+                          </li>
+                          
+                          <li >
+                            <a href="service.php">Services</a>
+                          </li>
+                          <!-- <li>
+                            <a href="#">Gallery</a>
+                          </li> -->
+                          <li >
+                            <a href="platform.php">Platform</a>
+                          </li>
+                          <li >
+                            <a href="contact.php">Contact Us</a>
+                          </li>
+                          <li class="current-menu-item">
+                            <a href="login.php">
+                            <button type="button" class="btn btn-primary btn-md login-btn">Login<i class="fas fa-sign-in-alt ml-5"></i></button> </a>
+                          </li>
+                        </ul>
+                        <!-- //.nav-menu -->
+                      </nav>
+                    </div>
+                    <!-- //.main-menu -->
+                  </div>
+                </div>
+                
+              </div>
+            </div>
+          </div>
+          <!-- Menu End -->
+        </header>
+        <!--Header End-->
+      </div><!-- Breadcrumbs Start -->
+<div class="rs-breadcrumbs breadcrumbs-overlay">
+    <div class="breadcrumbs-img login-breadcrumb">
+        <img src="assets/img/breadcrumb-img.jpg" alt="Breadcrumbs Image">
+    </div>
+    <div class="breadcrumbs-text white-color">
+        <h1 class="page-title">Login</h1>
+        <!-- <ul>
+            <li>
+                <a class="active" href="index.php">Home</a>
+            </li>
+            <li>Login</li>
+        </ul> -->
+    </div>
+</div>
+<div class="rs-login pt-50 pb-50 md-pt-50 md-pb-70">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-6">
+                <div class="noticed">
+                    <div class="main-part">
+                        <div class="method-account">
+                            <h2 class="login">Admin Login</h2>
+                            <form>
+                                <input type="text" name="username" placeholder="User Name" required="">
+                                <input type="text" name="text" placeholder="Password" required="">
+                                <button type="submit" class="readon submit-btn">login</button>
+                                <!-- <div class="last-password">
+                                    <p>Not registered? <a href="#">Create an account</a></p>
+                                </div> -->
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 md-mb-50">
+                <div class="noticed">
+                    <div class="main-part">
+                        <div class="method-account">
+                            <h2 class="login">Employee Login</h2>
+                            <form>
+                                <input type="text" name="User Name" placeholder="User Name" required="">
+                                <input type="text" name="username" placeholder="Password" required="">
+                                <button type="submit" class="readon submit-btn">login</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-12">
+                <div class="noticed">
+                    <div class="main-part">
+                        <div class="method-account">
+                            <h2 class="login">LMS Login</h2>
+                               <a href="https://learn.logicwhile.com/auth?slug=logicwhile" target="_blank"> <button type="submit" class="readon submit-btn"> login</button> </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Footer Start -->
+<footer id="rs-footer" class="rs-footer home9-style main-home rs-footer">
+  <div class="footer-top">
+    <div class="container">
+      <div class="row">
+        <!-- <div class="col-lg-4 col-md-12 col-sm-12 footer-widget md-mb-50"><h3 class="widget-title">About</h3><div class="textwidget pr-60 md-pr-15"><p class="white-color">To be the premier destination for students seeking comprehensive and industry-aligned training, bridging the gap between education and employment</p></div></div> -->
+        <div class="col-lg-12 col-md-6 col-sm-12 md-pl-15 footer-widget
+        md-mb-50">
+          <h3 class="widget-title">Services</h3>
+          <ul class="site-map mobile-services" style="display: inline-flex; margin-bottom: 40px;">
+            <li>
+              <a href="service.php"> Academic Training</a>
+            </li>
+            <li>
+              <a href="service.php"> CRT (Campus Recruitment Training)</a>
+            </li>
+            <li>
+              <a href="service.php"> Internships</a>
+            </li>
+            <li>
+              <a href="service.php"> SoC (Skill-oriented Course)</a>
+            </li>
+            <li>
+              <a href="service.php"> Projects</a>
+            </li>
+            <li>
+              <a href="service.php"> Workshops</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-12 footer-widget md-mb-50">
+          <h3 class="widget-title">Registred Office</h3>
+          <ul class="address-widget">
+            <li>
+              <i class="flaticon-location"></i>
+              <div class="desc">LogicWhile, 9-5/14, First Floor, Keshava Nagar, Boduppal, Medipally, Hyderabad, <br> Telangana – 500092</div>
+            </li>
+            <!-- <li>
+              <i class="flaticon-call"></i>
+              <div class="desc">
+                <a href="tel:+91 9985630896">+91 9985630896</a>
+              </div>
+            </li> -->
+            <!-- <li>
+              <i class="flaticon-email"></i>
+              <div class="desc">
+                <a href="mailto:support@logicwhile.com">support@logicwhile.com</a>
+              </div>
+            </li> -->
+            <li>
+              <i class="flaticon-location"></i>
+              <div class="desc">
+                <a href="https://maps.app.goo.gl/3x4B2ZoDjTP2bNgF8" target="_blank"> View on map
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-12 footer-widget md-mb-50">
+          <h3 class="widget-title">HYDERABAD (HEAD) OFFICE</h3>
+          <ul class="address-widget">
+            <li>
+              <i class="flaticon-location"></i>
+              <div class="desc">LogicWhile, LIG 80, First Floor, Beside MRO Office, <br> I-Phase KPHB Colony, Hyderabad, Telangana – 500072</div>
+            </li>
+            <li class="mt-3">
+              <i class="flaticon-location"></i>
+              <div class="desc">
+                <a href="https://maps.app.goo.gl/zQxwpfQCzbzZDyYK9" target="_blank"> View on map
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-12 footer-widget md-mb-50">
+          <h3 class="widget-title">Vijayawada Office</h3>
+          <ul class="address-widget">
+            <li>
+              <i class="flaticon-location"></i>
+              <div class="desc">LogicWhile, 61-312, 2nd Floor, Beside IDFC, Balaji Nagar, KrishnaLanka, Vijayawada, Andhra Pradesh - 520013</div>
+            </li>
+            <li class="mt-3">
+              <i class="flaticon-location"></i>
+              <div class="desc">
+                <a href="https://maps.app.goo.gl/ZC9haPPejXgUwFZW9" target="_blank"> View on map
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div class="col-lg-3 col-md-12 col-sm-12 footer-widget md-mb-50">
+          <h3 class="widget-title">Get in touch</h3>
+          <ul class="address-widget">
+            <li>
+              <i class="flaticon-call"></i>
+              <div class="desc">
+                <a href="tel:+91 9985630896">+91 9985630896</a>
+              </div>
+            </li>
+            <li>
+              <i class="flaticon-email"></i>
+              <div class="desc">
+                <a href="mailto:logicwhile@gmail.com">logicwhile@gmail.com</a>
+              </div>
+            </li>
+            <li>
+              <i class="flaticon-email"></i>
+              <div class="desc">
+                <a href="mailto:support@logicwhile.com">support@logicwhile.com</a>
+              </div>
+            </li>
+            <li>
+              <i class="far fa-arrow-alt-circle-down"></i>
+              <div class="desc">
+                <a href="assets/img/brochure.pdf" target="_blank">Download Brochure</a>
+              </div>
+            </li>
+            <li>
+              <i class="far fa-arrow-alt-circle-down"></i>
+              <div class="desc">
+                <a href="assets/img/qr-code.png" target="_blank">Brochure QR Code</a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="mobile--btns d-xl-block d-lg-block d-md-block d-block">
+   <div class="btnicon-whatsapp">
+      <a href="https://api.whatsapp.com/send?phone=9985630896" target="_blank">
+      <i class="fab fa-whatsapp"></i>
+      </a>
+   </div>
+   <div class="btnicon-call">
+      <a href="tel:+919985630896" target="#">
+      <i class="fas fa-phone-volume"></i>
+      </a>
+   </div>
+</div>
+  <div class="footer-bottom">
+    <div class="container">
+      <div class="row y-middle">
+        <div class="col-lg-6 md-mb-20">
+          <div class="copyright">
+            <p>&copy; 2024 LogicWhile.  All Rights Reserved.
+            </p>
+          </div>
+        </div>
+        <div class="col-lg-6 text-right md-text-left">
+          <ul class="footer-social footer-widget__social">
+
+
+
+            <!-- <li>
+              <a href="#"  class="color-1">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+            </li> -->
+            <li>
+              <a href="https://www.instagram.com/logicwhile/" target="_blank" class="color-2">
+                <i class="fab fa-instagram"></i>
+              </a>
+            </li>
+            <!-- <li>
+              <a href="#" class="color-3">
+                <i class="fab fa-twitter"></i>
+              </a>
+            </li> -->
+            <!-- <li>
+              <a href="#" class="color-4">
+                <i class="fab fa-youtube"></i>
+              </a>
+            </li> -->
+            <li>
+              <a href="https://www.linkedin.com/company/logicwhile/about/?viewAsMember=true" target="_blank" class="color-5">
+                <i class="fab fa-linkedin-in"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+<div id="scrollUp" class="orange-color">
+  <i class="fa fa-angle-up"></i>
+</div>
+<div aria-hidden="true" class="modal fade search-modal" role="dialog" tabindex="-1">
+  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+    <span class="flaticon-cross"></span>
+  </button>
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="search-block clearfix">
+        <form>
+          <div class="form-group">
+            <input class="form-control" placeholder="Search Here..." type="text">
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+<script src="assets/js/modernizr-2.8.3.min.js"></script>
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/rsmenu-main.js"></script>
+<script src="assets/js/jquery.nav.js"></script>
+<script src="assets/js/owl.carousel.min.js"></script>
+<script src="assets/js/slick.min.js"></script>
+<script src="assets/js/isotope.pkgd.min.js"></script>
+<script src="assets/js/imagesloaded.pkgd.min.js"></script>
+<script src="assets/js/wow.min.js"></script>
+<script src="assets/js/skill.bars.jquery.js"></script>
+<script src="assets/js/jquery.counterup.min.js"></script>
+<script src="assets/js/waypoints.min.js"></script>
+<script src="assets/js/jquery.mb.YTPlayer.min.js"></script>
+<script src="assets/js/jquery.magnific-popup.min.js"></script>
+<script src="assets/js/plugins.js"></script>
+<script src="assets/js/contact.form.js"></script>
+<script src="assets/js/main.js"></script>
+
+<script>
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+</script>
+</body>
+</html>
---
+++ pradeep_B/java script/intro.js
@@ -0,0 +1,58 @@
+// console.log("pradeep");
+// // alert("Pradeep");
+// // prompt("Enter name");
+
+// var a = 10;
+// let b = 20;
+// const pi = 3.142;
+// console.log(a);
+// console.log(b);
+// console.log(pi);
+// a = 20;
+// b = 30;
+// // pi = 4;
+// console.log(a);
+// console.log(b);
+// console.log(pi);
+
+// const c = "pradeep"
+// console.log(c);
+
+// // cannot re-declare let,
+// // can re-decclare var
+// var a = 20;
+// // let b = 30; will give error
+
+// console.log(a);
+// b = "pradeep";
+// console.log(b);
+
+// // assign value while declaring varible
+// let num1 = 20;
+// let num2;
+
+// var num3;
+// var num4 = 10;
+
+// num2 = 25;
+
+// num3 = 35;
+// console.log( num1,num2,num3,num4);
+
+
+let  a = 111;
+console.log(typeof a);
+
+let b = "hi";
+console.log(typeof b);
+
+let c = true ;
+console.log(typeof c);
+
+let d;
+console.log(typeof d);
+
+let e = undefined;
+console.log(typeof e);
+
+
