1e764bcf70f51d23bd514426c6eeba585cc1de93
karthik <karthiknaidu451@gmail.com>
2025-10-25 15:11:17 +0530

    Updated by karthik

---
+++ karthik_T/javascript/intro.js
@@ -0,0 +1,50 @@
+//console.log("Hi,Karthik")
+//alert("Hi  Nana")
+//prompt("Say Goodmorning Ra Thammudu")
+var a = 10;
+let b = 20;
+const pi = 3.142;
+console.log(a, b, pi);
+
+a = 20;
+b = 30;
+//pi = 3.14;
+console.log(a, b, pi);
+
+
+const c = "Karthik";
+console.log(c);
+
+var a = 20;
+//let b = 30;
+console.log(a, b);
+
+b = "karthik";
+console.log(b);
+
+//Assign the value while declaring variable
+let num1 = 20;
+let num2;
+
+let num3;
+let num4 = 10;
+console.log(num1, num2, num3, num4);
+num2 = 25;
+num3 = 35;
+console.log(num1, num2, num3, num4);
+
+a = 10;
+console.log(typeof a);
+
+let k = "Hi";
+console.log(typeof k);
+
+let t = true;
+console.log(typeof t);
+
+let u;
+console.log(typeof u);
+
+
+
+
