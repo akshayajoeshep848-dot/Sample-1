6123ac1a0511b15f322cc3fd8e48215d436b0d4b
Harika <madduharika29@gmail.com>
2025-10-28 15:11:39 +0530

    add harika folder

---
+++ harika_m/javascript/intro.js
@@ -0,0 +1,66 @@
+console.log("harika");
+// alert("hi harika");
+// prompt("please enter your name:");
+var a = 10;
+let b = 20;
+const pi = 3.142;
+console.log(a)
+console.log(b)
+console.log(pi)
+
+a = 20;
+b = 30;
+// pi = 4;
+
+const c = "harika";
+console.log(c)
+
+var a = 30;
+console.log(a)
+// cannot redeclare let,
+// can redeclare var,
+// let b=20; will give error.
+// we can assign let value.
+
+b = "harika";
+console.log(b)
+
+var a;
+console.log(a)
+
+// assign value while declaring variable
+let num1 = 20;
+let num2;
+
+var num3;
+var num4 = 10;
+console.log(num1, num2, num3, num4)
+
+num2 = 30;
+var num3 = 40;
+console.log(num1, num2, num3, num4)
+
+s = 20;      //we dont give keyword,soit can take directly var
+console.log(s)
+
+let h = 10;
+console.log(typeof h)
+
+let r = "harika";
+console.log(typeof b)
+
+let i = true;
+console.log(typeof i)
+
+let k;
+console.log(typeof k)
+
+let z = undefined;
+console.log(typeof z)
+
+
+
+
+
+
+
