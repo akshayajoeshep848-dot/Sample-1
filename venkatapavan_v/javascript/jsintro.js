2111e9c5da838c75f6482d73ac1a14e034881c48
Venkata Pavan <chintuvankayalapati127@gmail.com>
2025-10-28 17:52:31 +0530

    Add venkatapavan folders

---
+++ venkatapavan_v/javascript/jsintro.js
@@ -0,0 +1,49 @@
+      console.log("hi","pavan");
+       // alert("hello");
+      // prompt();
+
+var a = 10;
+let b = 20;
+const pi = 3.14;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+
+a = 20;
+b = 30;
+//pi = 4;
+
+
+console.log(a);
+console.log(b);
+console.log(pi);
+
+
+const c="pavan";
+console.log(c);
+
+
+// cannot re-declare let,
+// can re-declare var
+var a = 20;
+// let b = 30;will give error
+
+console.log(a);
+b="Pavan";
+console.log(b);
+
+// assign value while declaring variable
+let num1 = 20;
+let num2;
+
+
+var num3;
+var num4 = 10;
+console.log(num1, num2, num3, num4);
+
+
+num2=25;
+num3=35;
+console.log(num1, num2, num3, num4)
