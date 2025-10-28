de8f795fe8fc371fe5df813a5bd6a7438588e571
sujitha <bijinepallisujitha2003@gmail.com>
2025-10-28 15:37:10 +0530

    added sujitha folders

---
+++ sujitha_b/Java script/intro.js
@@ -0,0 +1,53 @@
+// console.log("sujitha");
+// // alert("hi sujitha");
+// //prompt();
+// var a = 10;
+// let b = 20;
+// const pi = 3.142;
+
+// console.log(a);
+// console.log(b);
+// console.log(pi);
+
+// a = 20;
+// b = 30;
+// //pi = 4;
+
+// console.log(a);
+// console.log(b);
+// console.log(pi);
+
+// const c = "sujitha";
+// console.log(c);
+
+// //cannot re-declare let,
+// //ca re-declare var
+// var a = 20;
+// //let b=30;will give error
+
+// console.log(a);
+// b = "sujitha";
+// console.log(b);
+
+
+//assign value while declaring variable
+let num1 = 20;
+let num2;
+
+var num3;
+var num4 = 10;
+console.log(num1, num2, num3, num4);
+
+num2 = 25;
+num3 = 35;
+console.log(num1, num2, num3, num4);
+
+let b = "Hi";
+console.log(typeof b);
+
+let d;
+console.log(typeof d);
+
+let e = undefined;
+
+console.log(typeof e);
