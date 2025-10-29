10ec2fe48642868c7db6e43d437c748c850acdd9
mannepallimanoj076-blip <mannepallimanoj076@gmail.com>
2025-10-29 09:41:28 +0530

    commiting

---
+++ manoj_m/html/jsintro.js
@@ -0,0 +1,44 @@
+console.log("this is manoj");
+//alert("hi,manoj"); //
+ //prompt("enter your name");
+ var a = 10;
+ let b = 20;
+ const pi = 3.142;
+
+ console.log(a)
+ console.log(b)
+ console.log(pi)
+
+ a=10;
+ b=20;
+  //pi=4;
+ console.log(a)
+ console.log(b)
+ console.log(pi)
+
+ const c = "manoj"
+ console.log(c);
+  
+ //cannot re-declare let,
+ // can re-declare var
+ var a=20;
+ // let b =30; will give error
+ console.log(a);
+ b="manoj";
+ console.log(b);
+
+ //assign value while declaring variable
+ let num1=20;
+ let num2=30;
+ var num3;
+ var num4=10;
+ console.log(num1,num2,num3,num4);
+
+ num2=25;
+ num3=35;
+ console.log(num1,num2,num3,num4);
+
+ num5=10;
+ console.log(num5);
+
+ 
---
+++ manoj_m/js/var.js
@@ -0,0 +1,14 @@
+let a = 50;
+ console.log(typeof a);
+
+ let b = "hi";
+ console.log(typeof b);
+
+let c = true;
+console.log(typeof c);
+
+let d;
+console.log(typeof d);
+
+let e = undefined;
+console.log(typeof e);
