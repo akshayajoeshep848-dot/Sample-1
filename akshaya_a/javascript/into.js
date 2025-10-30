46ca5a6f7ea5cd765dcdf20e55ae3768cb85d0c5
akshaya <akshayajoeshep848@gmail.com>
2025-10-30 11:47:29 +0530

    akshaya added folder

---
+++ akshaya_a/javascript/into.js
@@ -0,0 +1,72 @@
+// console.log("this is akshaya");
+// prompt()
+// alert("hii")
+// var a =10;
+// let b=20;
+// const pi=3.142;
+// console.log(a);
+// console.log(b);
+// console.log(pi);
+
+
+// a=20;
+// b=30;
+// // pi=4;
+// console.log(a);
+// console.log(b);
+// console.log(pi);
+
+// const c="akshaya";
+// console.log(c);
+
+// //cannot re-declared let,
+// //can re-declare var
+// var a =20;
+// //let b=30; will give a error
+// console.log(a);
+// b="akshaya";
+// console.log(b);
+
+// //assign value while declaring varable
+// // let nm1=20;
+// // let nm2;
+// // var nm3;
+// // var nm4=10;
+// // console.log(nm1,nm2,nm3,nm4);
+
+// // nm4=10;
+// // console.log(nm4)
+
+//  a=10;
+//  b=40;
+//  console.log(a+b)
+// let a =10;
+// console.log(typeof a) ;
+
+// let b="hii";
+// console.log(typeof b);
+
+// let c = true;
+// console.log(typeof c);
+
+// let d;
+// console.log(typeof d);
+
+// let e= undefined;
+// console.log(typeof e);
+
+// g=20;
+// f=30;
+// console.log(g+f)
+
+var ak=20;
+const as=35;
+let ah=45;
+console.log(ak);
+console.log(as);
+console.log(ah);
+
+var ak=43;
+console.log(ak);
+let az=45;
+console.log(az);
